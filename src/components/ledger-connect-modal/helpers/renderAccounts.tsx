4db6e6fe844a5be92dc39a3b9086bf2079b8f30c
Tudor Morar <tudor.morar@multiversx.com>
2024-11-28 15:11:34 +0200

    Remove big number reference

--- src/components/ledger-connect-modal/helpers/renderAccounts.tsx
+++ src/components/ledger-connect-modal/helpers/renderAccounts.tsx
@@ -1,4 +1,3 @@
-import BigNumber from 'bignumber.js';
 import { h } from '@stencil/core';
 import { ILedgerAccount } from '../ledger-connect-modal.types';
 import { DataTestIdsEnum } from 'constants/dataTestIds.enum';
@@ -7,14 +6,6 @@ export function trimAddress(s: string): string {
   const firstFour = s.slice(0, 6);
   const lastFour = s.slice(-6);
   return `${firstFour}...${lastFour}`;
-}
-
-export function formatAmount(amount: string): string {
-  const number = new BigNumber(amount);
-  if (number.isNaN()) {
-    return amount;
-  }
-  return number.dividedBy(BigNumber(10).pow(18)).toFormat(4).toString();
 }
 
 interface IRenderAccountsProps {
@@ -34,7 +25,7 @@ export const renderAccounts = ({ shownAccounts = [], onSelectAccount, selectedIn
         <div class="account-row" onClick={() => onSelectAccount(account.index)}>
           <input type="radio" name="account" checked={account.index === selectedIndex} value={account.index} />
           <span class="address">{trimAddress(account.address)}</span>
-          <span class="balance">{formatAmount(account.balance ?? '')}</span>
+          <span class="balance">{account.balance}</span>
           <span class="index">{account.index}</span>
         </div>
       ))}
