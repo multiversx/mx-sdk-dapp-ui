b82c9773cdcd1fc99416bfcae185d52d67fd912e
Gavrila Andrei <andrei.gavrila@multiversx.com>
2025-05-14 13:15:12 +0300

    Add decode data field

--- src/components/functional/sign-transactions-panel/components/sign-transactions-advanced/sign-transactions-advanced.tsx
+++ src/components/functional/sign-transactions-panel/components/sign-transactions-advanced/sign-transactions-advanced.tsx
@@ -1,5 +1,10 @@
 import { Component, h, Prop, State } from '@stencil/core';
 
+import { DecodeMethodEnum } from '../../sign-transactions-panel.types';
+import state from '../../signTransactionsPanelStore';
+
+const DECODE_METHODS = Object.values(DecodeMethodEnum);
+
 @Component({
   tag: 'mvx-sign-transactions-advanced',
   styleUrl: 'sign-transactions-advanced.css',
@@ -9,12 +14,27 @@ export class SignTransactionsAdvanced {
   @Prop() highlight?: string;
 
   @State() activeSpeed: string = 'Standard';
+  @State() decodeMethod: DecodeMethodEnum = DecodeMethodEnum.raw;
 
   setActiveSpeed(speed: string) {
     this.activeSpeed = speed;
   }
 
-  getHighlightedData() {
+  setDecodeMethod(method: DecodeMethodEnum) {
+    this.decodeMethod = method;
+  }
+
+  get computedDisplayData() {
+    if (this.decodeMethod !== DecodeMethodEnum.raw) {
+      const {
+        commonData: { decodedData },
+      } = state;
+
+      const currentData = decodedData?.[this.decodeMethod];
+
+      return currentData?.displayValue;
+    }
+
     if (!this.highlight || !this.data.includes(this.highlight)) {
       return this.data;
     }
@@ -55,9 +75,18 @@ export class SignTransactionsAdvanced {
         </div>
 
         <div class="data-section">
-          <div class="data-label">Data</div>
-          <div class="data-content">
-            <span class="data-text">{this.getHighlightedData()}</span>
+          <select class="data-decode-method-select" onInput={(e: Event) => this.setDecodeMethod((e.target as HTMLSelectElement).value as DecodeMethodEnum)}>
+            {DECODE_METHODS.map(method => (
+              <option value={method} selected={this.decodeMethod === method}>
+                {method.toUpperCase()}
+              </option>
+            ))}
+          </select>
+          <div class="data-field">
+            <div class="data-label">Data</div>
+            <div class="data-content">
+              <span class="data-text">{this.computedDisplayData}</span>
+            </div>
           </div>
         </div>
       </div>
