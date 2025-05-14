b6fb9bdea2a21400af449297afd4610163296271
Gavrila Andrei <andrei.gavrila@multiversx.com>
2025-05-14 11:31:48 +0300

    Add isLoading state (#96)
    * Add isLoading state
    
    * Revert change
    
    * Updates
    
    * Remove isLoading from initState

--- src/components/functional/sign-transactions-panel/sign-transactions-panel.types.ts
+++ src/components/functional/sign-transactions-panel/sign-transactions-panel.types.ts
@@ -51,6 +51,7 @@ export interface ISignTransactionsPanelCommonData {
 
 export interface ISignTransactionsPanelData {
   shouldClose?: boolean;
+  isLoading?: boolean;
   commonData: ISignTransactionsPanelCommonData;
   tokenTransaction: {
     identifier?: string;
