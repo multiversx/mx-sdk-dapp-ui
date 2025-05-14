c3d850264414bc0f7e02ec16449cfd173bd8db55
iuliacimpeanu <iuliaalexandra.ignat@gmail.com>
2025-05-14 15:49:14 +0300

    added unit tests for getAmount function

--- src/components/functional/toasts-list/helpers/getAmountParts/getAmountParts.ts
+++ src/components/functional/toasts-list/helpers/getAmountParts/getAmountParts.ts
@@ -1,10 +1,19 @@
 export const getAmountParts = (amount = '') => {
-  const [value, label] = amount.split(' ');
+  if (amount == null) {
+    return { amountInteger: '', amountDecimal: '', label: '' };
+  }
+
+  const [value, label = ''] = amount.split(' ');
+
+  if (isNaN(Number(value))) {
+    return { amountInteger: '', amountDecimal: '', label };
+  }
+
   const [amountInteger, amountDecimal] = value.split('.');
 
   return {
     amountInteger,
-    amountDecimal: `.${amountDecimal}`,
+    amountDecimal: amountDecimal ? `.${amountDecimal}` : '',
     label,
   };
 };
