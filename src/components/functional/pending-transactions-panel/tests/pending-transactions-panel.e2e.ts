c2f12c6bace477e3b175211de09036cf95b94d75
iuliacimpeanu <iuliaalexandra.ignat@gmail.com>
2025-05-16 18:41:26 +0300

    fixed failing test

--- src/components/functional/pending-transactions-panel/tests/pending-transactions-panel.e2e.ts
+++ src/components/functional/pending-transactions-panel/tests/pending-transactions-panel.e2e.ts
@@ -1,9 +1,7 @@
 import { newE2EPage } from '@stencil/core/testing';
-import { DataTestIdsEnum } from 'constants/dataTestIds.enum';
 
 const tag = 'mvx-pending-transactions-panel';
-const title = 'Pending Transaction';
-const subtitle = 'Confirm transaction on wallet';
+const title = 'Confirm on MultiversX DeFi wallet';
 
 describe('pending-transactions-panel', () => {
   it(`renders ${tag}`, async () => {
@@ -19,13 +17,11 @@ describe('pending-transactions-panel', () => {
 
     await page.setContent(`<${tag}></${tag}>`);
     const component = await page.find(tag);
-    component.setProperty('data', { title, subtitle });
+    component.setProperty('data', { title });
 
     await page.waitForChanges();
-    const titleElement = await page.find(`[data-testid=${DataTestIdsEnum.pendingTransactionsTitle}]`);
-    expect(titleElement.textContent).toContain(title);
-
-    const subtitleElement = await page.find(`[data-testid=${DataTestIdsEnum.pendingTransactionsSubtitle}]`);
-    expect(subtitleElement.textContent).toContain(subtitle);
+    const panel = await page.find('mvx-side-panel');
+    const titleAttr = await panel.getProperty('panelTitle');
+    expect(titleAttr).toBe(title);
   });
 });
