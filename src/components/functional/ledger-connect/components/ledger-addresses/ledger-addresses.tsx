712eb5e7915cacda96c56803ec7c1ce9fcbe5505
iuliacimpeanu <iuliaalexandra.ignat@gmail.com>
2025-06-18 17:21:54 +0300

    small UI fixes

--- src/components/functional/ledger-connect/components/ledger-addresses/ledger-addresses.tsx
+++ src/components/functional/ledger-connect/components/ledger-addresses/ledger-addresses.tsx
@@ -70,6 +70,7 @@ export class LedgerAddresses {
         'mvx:w-13!': isIndexInTheHundreds,
         'mvx:w-17!': isIndexInTheThousands,
       }),
+      spinner: 'mvx:fill-accent!',
     };
 
     if (isAddressesLoadingInitially) {
@@ -179,7 +180,7 @@ export class LedgerAddresses {
             class={{ 'ledger-addresses-button': true, 'loading': isPageChanging, 'disabled': isAccessWalletDisabled }}
           >
             <span class="ledger-addresses-button-label">{isPageChanging ? 'Loading Wallets...' : 'Access Wallet'}</span>
-            {isPageChanging && <mvx-spinner-icon />}
+            {isPageChanging && <mvx-spinner-icon class={ledgerAddressesClasses.spinner} />}
           </button>
         </div>
       </div>
