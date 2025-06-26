cc105af293c23dfed67394c9fd2cef54e2c82728
Miro Mărgineanu <miro.margineanu@multiversx.com>
2025-06-26 18:09:45 +0300

    Updated styles

--- src/components/functional/wallet-connect/wallet-connect.tsx
+++ src/components/functional/wallet-connect/wallet-connect.tsx
@@ -15,9 +15,11 @@ import { WalletConnectEventsEnum } from './wallet-connect.types';
 export class WalletConnect {
   private eventBus: IEventBus = new EventBus();
 
-  @Prop() data: IWalletConnectPanelData = { wcURI: '' };
   @State() showScanPage: boolean = true;
+  @State() walletConnectDeepLink: string = '';
   @Element() hostElement: HTMLElement;
+
+  @Prop() data: IWalletConnectPanelData = { wcURI: '' };
   @Prop() qrCodeSvg: string = '';
 
   @Watch('data')
@@ -42,6 +44,10 @@ export class WalletConnect {
   }
 
   private async dataUpdate(payload: IWalletConnectPanelData) {
+    if (payload.walletConnectDeepLink) {
+      this.walletConnectDeepLink = payload.walletConnectDeepLink;
+    }
+
     if (payload.wcURI) {
       this.qrCodeSvg = await this.generateSVG(payload.wcURI);
     }
@@ -69,11 +75,11 @@ export class WalletConnect {
     return (
       <Fragment>
         <mvx-side-panel-header
-          panelTitle={providerLabels.walletConnect}
           hasRightButton={true}
           hasLeftButton={!this.showScanPage}
-          onRightButtonClick={() => this.eventBus.publish(WalletConnectEventsEnum.CLOSE)}
+          panelTitle={providerLabels.walletConnect}
           onLeftButtonClick={this.handlePageToggle.bind(this)}
+          onRightButtonClick={() => this.eventBus.publish(WalletConnectEventsEnum.CLOSE)}
         >
           {!this.showScanPage && <mvx-back-arrow-icon slot={SidePanelHeaderSlotEnum.leftIcon} />}
           <mvx-close-icon slot={SidePanelHeaderSlotEnum.rightIcon} />
@@ -81,7 +87,11 @@ export class WalletConnect {
 
         <div class="wallet-connect">
           {this.showScanPage ? (
-            <mvx-wallet-connect-scan qrCodeSvg={this.qrCodeSvg} onDownloadClick={this.handlePageToggle.bind(this)} />
+            <mvx-wallet-connect-scan
+              qrCodeSvg={this.qrCodeSvg}
+              onDownloadClick={this.handlePageToggle.bind(this)}
+              walletConnectDeepLink={this.walletConnectDeepLink}
+            />
           ) : (
             <mvx-wallet-connect-download />
           )}
