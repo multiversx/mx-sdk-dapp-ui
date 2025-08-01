b7c31070b126f75407ce1427646c2996ac1d0acf
Miro Mărgineanu <miro.margineanu@multiversx.com>
2025-08-01 13:27:52 +0300

    Fixed Storybook story theme

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,21 +1,23 @@
 /** @jsx h */
-import type { Preview, StencilRenderer, StoryContext } from '@stencil/storybook-plugin';
-import { PartialStoryFn } from 'storybook/internal/csf';
-import { h } from '@stencil/core';
+import type { Preview } from '@stencil/storybook-plugin';
 
 import { defineCustomElements } from '../dist/web-components';
 
 import '../src/global/variables.css';
-import '../src/global/style.css';
+import './storybook-styles.css';
 
 defineCustomElements();
 
 export const parameters: Preview['parameters'] = {
   backgrounds: {
     default: 'dark',
-    options: {
-      dark: { name: 'Dark', value: '#171717' },
-      light: { name: 'Light', value: '#f3efed' },
-    },
+    values: [
+      { name: 'Dark', value: '#171717' },
+      { name: 'Light', value: '#f3efed' },
+    ],
   },
+};
+
+export const initialGlobals: Preview['initialGlobals'] = {
+  backgrounds: { value: 'dark' },
 };
