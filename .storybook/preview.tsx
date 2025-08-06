a4cb3331b97de565d7bafe4ea893370d8175902d
Miro <miro.margineanu@multiversx.com>
2025-08-06 15:40:51 +0300

    Added Storybook (#180)
    * Added Storybook
    
    * Removed unnecessary Vite configurations
    
    * Removed unused files
    
    * Updated Storybook
    
    * Fixed Storybook story theme
    
    * Updated generic button structure
    
    * Revert "Updated generic button structure"
    
    This reverts commit 64453f56ffc9d299f7408cdea834e3f3f6cc770b.
    
    * Updated Button structure
    
    * Fixed Storybook build
    
    * Updated Storybook layout
    
    * Updated CHANGELOG.md
    
    * Fixed Tailwind in Storybook
    
    * Remove deprecated build workflow and update npm publish and pre-merge unit tests workflows to improve permissions handling and Node.js setup.
    
    * Update pnpm-lock.yaml and remove yarn.lock; added new dependencies for Storybook and Stencil, and updated existing package versions.
    
    * Add Chrome installation for Puppeteer in pre-merge unit tests workflow
    
    * Updated styles
    
    * Removed unnecessary assets
    
    * Fixed pnpm-lock.yaml file
    
    ---------
    
    Co-authored-by: Razvan Tomegea <razvan.tomegea@gmail.com>

---
+++ .storybook/preview.tsx
@@ -0,0 +1,33 @@
+/** @jsx h */
+import { h } from '@stencil/core';
+import type { Preview } from '@stencil/storybook-plugin';
+
+import { defineCustomElements } from '../dist/web-components';
+
+import '../src/global/variables.css';
+import './styles.css';
+import './tailwind.css';
+
+defineCustomElements();
+
+export const decorators: Preview['decorators'] = [
+  (Story, context) => (
+    <div data-mvx-theme={`mvx:${context.globals.backgrounds.value}-theme`}>
+      <Story />
+    </div>
+  ),
+];
+
+export const initialGlobals: Preview['initialGlobals'] = {
+  backgrounds: { value: 'dark' },
+};
+
+export const parameters: Preview['parameters'] = {
+  backgrounds: {
+    default: 'dark',
+    values: [
+      { name: 'Dark', value: '#171717' },
+      { name: 'Light', value: '#f3efed' },
+    ],
+  },
+};
