4e5ed5a199ecc0389d0daec3e4feebb08ddfcc02
Tudor Morar <tudor.morar@multiversx.com>
2025-09-19 15:26:09 +0300

    revert

--- src/utils/icons/getIconHtmlFromIconDefinition.ts
+++ src/utils/icons/getIconHtmlFromIconDefinition.ts
@@ -1,5 +1,5 @@
 import type { Icon, IconDefinition } from '@fortawesome/fontawesome-svg-core';
-import { icon } from '@fortawesome/fontawesome-svg-core';
+import { icon, library } from '@fortawesome/fontawesome-svg-core';
 
 export function getIconHtmlFromIconDefinition(iconDefinition: IconDefinition): string | null {
   if (!iconDefinition) {
@@ -7,6 +7,7 @@ export function getIconHtmlFromIconDefinition(iconDefinition: IconDefinition): s
   }
 
   let faIcon: Icon;
+  library.add(iconDefinition);
   faIcon = icon(iconDefinition);
 
   return faIcon ? faIcon.html[0] : null;
