cf25608da0ac06bdd346c5e1e1100a0280ea3e23
Tudor Morar <tudor.morar@multiversx.com>
2025-09-19 10:38:30 +0300

    Revert fa-icon

--- src/components/visual/fa-icon/fa-icon.tsx
+++ src/components/visual/fa-icon/fa-icon.tsx
@@ -1,5 +1,5 @@
 import type { IconDefinition } from '@fortawesome/free-solid-svg-icons';
-import { Component, h, Prop, State, Watch } from '@stencil/core';
+import { Component, h, Prop } from '@stencil/core';
 import { getIconHtmlFromIconDefinition } from 'utils/icons/getIconHtmlFromIconDefinition';
 import { getIconHtmlFromIconName } from 'utils/icons/getIconHtmlFromIconName';
 
@@ -11,34 +11,15 @@ export class FaIcon {
   @Prop() class?: string = 'fa-icon';
   @Prop() icon: IconDefinition | string;
   @Prop() description?: string;
-  @State() private iconHtml?: string | null;
-
-  async componentWillLoad() {
-    await this.updateIconHtml(this.icon);
-  }
-
-  @Watch('icon')
-  async onIconChange(newValue: IconDefinition | string) {
-    await this.updateIconHtml(newValue);
-  }
-
-  private async updateIconHtml(icon: IconDefinition | string) {
-    if (!icon) {
-      this.iconHtml = null;
-      return;
-    }
-    if (typeof icon === 'string') {
-      this.iconHtml = await getIconHtmlFromIconName(icon);
-      return;
-    }
-    this.iconHtml = getIconHtmlFromIconDefinition(icon);
-  }
 
   render() {
     if (!this.icon) {
       return null;
     }
 
-    return <i class={this.class} innerHTML={this.iconHtml} title={this.description}></i>;
+    const iconHtml =
+      typeof this.icon === 'string' ? getIconHtmlFromIconName(this.icon) : getIconHtmlFromIconDefinition(this.icon);
+
+    return <i class={this.class} innerHTML={iconHtml} title={this.description}></i>;
   }
 }
