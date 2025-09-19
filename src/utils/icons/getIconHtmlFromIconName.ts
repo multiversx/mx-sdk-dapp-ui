989c5ad2d36a66717802c1344335da446b1e0b64
Tudor Morar <tudor.morar@multiversx.com>
2025-09-19 15:25:49 +0300

    revert

--- src/utils/icons/getIconHtmlFromIconName.ts
+++ src/utils/icons/getIconHtmlFromIconName.ts
@@ -1,32 +1,21 @@
-import type { Icon, IconDefinition, IconName } from '@fortawesome/fontawesome-svg-core';
-import { icon } from '@fortawesome/fontawesome-svg-core';
+import type { Icon, IconName } from '@fortawesome/fontawesome-svg-core';
+import { icon, library } from '@fortawesome/fontawesome-svg-core';
+import * as solidIcons from '@fortawesome/free-solid-svg-icons';
 
-const iconCache = new Map<string, IconDefinition>();
-
-export async function getIconHtmlFromIconName(iconName: IconName | string): Promise<string | null> {
+export function getIconHtmlFromIconName(iconName: IconName | string): string | null {
+  let faIcon: Icon;
   const usedIconName = iconName.includes('fa') ? iconName : `fa${capitalize(iconName)}`;
+  const dynamicIcon = solidIcons[usedIconName];
 
-  try {
-    let iconDefinition: IconDefinition | undefined = iconCache.get(usedIconName);
-    if (!iconDefinition) {
-      const module = await import(`@fortawesome/free-solid-svg-icons/${usedIconName}`);
-      iconDefinition = module?.[usedIconName] as IconDefinition | undefined;
-      if (iconDefinition) {
-        iconCache.set(usedIconName, iconDefinition);
-      }
-    }
-
-    if (!iconDefinition) {
-      console.error(`Icon "${iconName}" not found"`);
-      return null;
-    }
-
-    const faIcon: Icon = icon(iconDefinition);
-    return faIcon ? faIcon.html[0] : null;
-  } catch (err) {
-    console.error(`Failed to load icon "${iconName}":`, err);
+  if (!dynamicIcon) {
+    console.error(`Icon "${iconName}" not found"`);
     return null;
   }
+
+  library.add(dynamicIcon);
+  faIcon = icon(dynamicIcon);
+
+  return faIcon ? faIcon.html[0] : null;
 }
 
 function capitalize(iconName: string): string {
