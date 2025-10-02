8a1a2d29ab7e3b0c74fd1cd8a1dd2e3702043499
Iulia Cimpeanu <iuliaalexandra.ignat@gmail.com>
2025-10-02 15:20:08 +0300

    Updated styles

--- src/components/controlled/transactions-table/components/transaction-method/transaction-method.tsx
+++ src/components/controlled/transactions-table/components/transaction-method/transaction-method.tsx
@@ -2,6 +2,14 @@ import { Component, h, Prop } from '@stencil/core';
 import classNames from 'classnames';
 import { DataTestIdsEnum } from 'constants/dataTestIds.enum';
 
+// prettier-ignore
+const styles = {
+  transactionMethodBadge: 'transaction-method-badge mvx:inline-block mvx:py-1 mvx:px-1.5 mvx:font-normal mvx:text-center mvx:whitespace-pre-wrap mvx:text-[75%] mvx:leading-[1] mvx:break-all mvx:align-baseline mvx:rounded-sm mvx:transition-colors duration-200 ease-in-out mvx:motion-reduce:transition-none mvx:text-transaction-method mvx:border-1 mvx:border-transaction-method mvx:bg-transparent mvx:py-1 mvx:px-2 mvx:rounded-sm font-light',
+  transactionMethodBadgeEmpty: 'transaction-method-badge-empty mvx:hidden',
+  transactionMethodText: 'transaction-method-text mvx:truncate mvx:capitalize'
+
+} satisfies Record<string, string>;
+
 @Component({
   tag: 'mvx-transaction-method',
   styleUrl: 'transaction-method.scss',
@@ -14,11 +22,15 @@ export class TransactionMethod {
   render() {
     return (
       <span
-        class={classNames('badge badge-secondary badge-pill font-weight-light transaction-method', this.class)}
+        class={classNames(
+          styles.transactionMethodBadge,
+          { [styles.transactionMethodBadgeEmpty]: this.method === '' },
+          this.class,
+        )}
         data-testid={DataTestIdsEnum.method}
         title={this.actionDescription}
       >
-        <div class="text-truncate text-capitalize">{this.method}</div>
+        <div class={styles.transactionMethodText}>{this.method}</div>
       </span>
     );
   }
--- src/components/controlled/transactions-table/components/transaction-shards/transaction-shards.tsx
+++ src/components/controlled/transactions-table/components/transaction-shards/transaction-shards.tsx
@@ -4,9 +4,11 @@ import { DataTestIdsEnum } from 'constants/dataTestIds.enum';
 
 import type { TransactionRowType } from '../../transactions-table.type';
 
-const transactionShardsClasses: Record<string, string> = {
-  explorerLink: 'mvx:text-primary!',
-};
+// prettier-ignore
+const styles = {
+  transactionShards :'transaction-shards mvx:items-center mvx:flex mvx:gap-2 mvx:w-max mvx:fill-label',
+  explorerLink: 'explorer-link mvx:text-primary!'
+} satisfies Record<string, string>;
 
 @Component({
   tag: 'mvx-transaction-shards',
@@ -18,23 +20,23 @@ export class TransactionShards {
 
   render() {
     return (
-      <div class={classNames(this.class, 'transaction-shards')}>
+      <div class={classNames(this.class, styles.transactionShards)}>
         <mvx-explorer-link
           link={this.transaction.sender.shardLink}
-          class={transactionShardsClasses.explorerLink}
+          class={styles.explorerLink}
           data-testid={DataTestIdsEnum.shardFromLink}
         >
           <span data-testid={DataTestIdsEnum.senderShard}>{this.transaction.sender.shard}</span>
         </mvx-explorer-link>
 
-        <span class="transaction-shards-arrow">
+        <span class={styles.transactionShards}>
           <mvx-arrow-right-icon />
         </span>
 
         <mvx-explorer-link
           link={this.transaction.receiver.shardLink}
           data-testid={DataTestIdsEnum.shardToLink}
-          class={transactionShardsClasses.explorerLink}
+          class={styles.explorerLink}
         >
           <span data-testid={DataTestIdsEnum.receiverShard}>{this.transaction.receiver.shard}</span>
         </mvx-explorer-link>
