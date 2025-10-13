c1d07fdddabea0db82997d7c1ab347c1c59377d2
Miro Mărgineanu <miro.margineanu@multiversx.com>
2025-10-13 16:23:33 +0300

    Fixed deploy

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,20 @@
+import path from 'path';
+
 const config = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-docs'],
   framework: {
     name: '@stencil/storybook-plugin',
+    options: {},
   },
-  staticDirs: ['../dist/web-components'],
+  staticDirs: ['../dist/web-components'], // <-- important
+  webpackFinal: async config => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@dist': path.resolve(__dirname, '../dist'),
+    };
+    return config;
+  },
 };
 
 export default config;
