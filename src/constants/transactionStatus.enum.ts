246807c5e56c9ad2d64340388b4ace2b8725b655
Iulia Cimpeanu <iuliaalexandra.ignat@gmail.com>
2025-10-20 13:53:28 +0300

    Updated enum

--- src/constants/transactionStatus.enum.ts
+++ src/constants/transactionStatus.enum.ts
@@ -2,5 +2,8 @@ export enum TransactionStatusEnum {
     success = 'success',
     pending = 'pending',
     fail = 'fail',
-    invalid = 'invalid'
+    invalid = 'invalid',
+    executed = 'executed',
+    notExecuted = 'not executed',
+    rewardReverted = 'reward-reverted'
 }
