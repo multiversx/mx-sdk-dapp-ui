21aed772e7714e47a6778449df675517e15ffe30
Iulia Cimpeanu <iuliaalexandra.ignat@gmail.com>
2025-10-21 17:16:14 +0300

    Refactored unlock panel footer

--- src/components/functional/unlock-panel/components/unlock-panel-footer/unlock-panel-footer.tsx
+++ src/components/functional/unlock-panel/components/unlock-panel-footer/unlock-panel-footer.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Prop } from '@stencil/core';
+import { h } from '@stencil/core';
 import { Icon } from 'common/Icon';
 
 import unlockPanelWalletImg from '../../../../../assets/unlock-panel-wallet.webp';
@@ -8,50 +8,45 @@ const styles = {
   unlockButton: 'unlock-panel-footer-icon mvx:text-primary mvx:w-4 mvx:h-auto mvx:hidden mvx:xs:flex mvx:ml-auto mvx:mt-auto',
 } satisfies Record<string, string>;
 
-@Component({
-  tag: 'mvx-unlock-panel-footer',
-  styleUrl: 'unlock-panel-footer.scss',
-  shadow: true,
-})
-export class UnlockPanel {
-  @Prop() walletAddress: string;
+export function UnlockPanelFooter({ walletAddress }: { walletAddress: string }) {
 
-  handleWalletClick = (event: MouseEvent) => {
+
+  const handleWalletClick = (event: MouseEvent) => {
     event.preventDefault();
     event.stopPropagation();
-    window.open(this.walletAddress, '_blank');
+    window.open(walletAddress, '_blank');
   };
 
-  render() {
-    const processedWalletAddress = String(this.walletAddress).replace('https://', '');
 
-    return (
-      <div class="unlock-panel-footer" onClick={this.handleWalletClick}>
-        <img src={unlockPanelWalletImg} class="unlock-panel-footer-image" />
+  const processedWalletAddress = String(walletAddress).replace('https://', '');
 
-        <div class="unlock-panel-footer-wrapper">
-          <div class="unlock-panel-footer-title">Don't have a wallet?</div>
+  return (
+    <div class="unlock-panel-footer" onClick={handleWalletClick}>
+      <img src={unlockPanelWalletImg} class="unlock-panel-footer-image" />
 
-          <div class="unlock-panel-footer-subtitle desktop">
-            Take full control of <br /> your assets.
-          </div>
+      <div class="unlock-panel-footer-wrapper">
+        <div class="unlock-panel-footer-title">Don't have a wallet?</div>
 
-          <div class="unlock-panel-footer-subtitle mobile">
-            <span>See which one to get on </span>
-
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              class="unlock-panel-footer-subtitle-link"
-              href={this.walletAddress}
-            >
-              {processedWalletAddress}
-            </a>
-          </div>
-
-          <Icon name="arrow-up-right" class={styles.unlockButton} />
+        <div class="unlock-panel-footer-subtitle desktop">
+          Take full control of <br /> your assets.
         </div>
+
+        <div class="unlock-panel-footer-subtitle mobile">
+          <span>See which one to get on </span>
+
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            class="unlock-panel-footer-subtitle-link"
+            href={walletAddress}
+          >
+            {processedWalletAddress}
+          </a>
+        </div>
+
+        <Icon name="arrow-up-right" class={styles.unlockButton} />
       </div>
-    );
-  }
+    </div>
+  );
+
 }
