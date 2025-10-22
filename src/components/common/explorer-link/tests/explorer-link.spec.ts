4cd7adb924e874e78f31bcfdb778c905f9ee658e
Iulia Cimpeanu <iuliaalexandra.ignat@gmail.com>
2025-10-22 14:13:23 +0300

    Fixed tests

--- src/components/common/explorer-link/tests/explorer-link.spec.ts
+++ src/components/common/explorer-link/tests/explorer-link.spec.ts
@@ -12,7 +12,11 @@ describe('ExplorerLink', () => {
 
     expect(page.root).toEqualHtml(`
       <mvx-explorer-link link="https://example.com">
-        <a class="explorer-link mvx:decoration-0 mvx:flex" href="https://example.com" rel="noreferrer" target="_blank"></a>
+        <a class="explorer-link mvx:decoration-0 mvx:flex" href="https://example.com" rel="noreferrer" target="_blank">
+          <svg class="explorer-link-icon mvx:duration-200 mvx:ease-in-out mvx:flex mvx:hover:opacity-80 mvx:justify-center mvx:transition-opacity" height="16" viewBox="0 0 512 512" width="16" xmlns="http://www.w3.org/2000/svg">
+            <path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l82.7 0L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3l0 82.7c0 17.7 14.3 32 32 32s32-14.3 32-32l0-160c0-17.7-14.3-32-32-32L320 0zM80 32C35.8 32 0 67.8 0 112L0 432c0 44.2 35.8 80 80 80l320 0c44.2 0 80-35.8 80-80l0-112c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 112c0 8.8-7.2 16-16 16L80 448c-8.8 0-16-7.2-16-16l0-320c0-8.8 7.2-16 16-16l112 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L80 32z" fill="currentColor"></path>
+          </svg>
+        </a>
       </mvx-explorer-link>
     `);
   });
@@ -26,7 +30,11 @@ describe('ExplorerLink', () => {
     expect(page.root).toEqualHtml(`
       <mvx-explorer-link link="https://example.com">
         View on Explorer
-        <a class="explorer-link mvx:decoration-0 mvx:flex" href="https://example.com" rel="noreferrer" target="_blank"></a>
+        <a class="explorer-link mvx:decoration-0 mvx:flex" href="https://example.com" rel="noreferrer" target="_blank">
+          <svg class="explorer-link-icon mvx:duration-200 mvx:ease-in-out mvx:flex mvx:hover:opacity-80 mvx:justify-center mvx:transition-opacity" height="16" viewBox="0 0 512 512" width="16" xmlns="http://www.w3.org/2000/svg">
+            <path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l82.7 0L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3l0 82.7c0 17.7 14.3 32 32 32s32-14.3 32-32l0-160c0-17.7-14.3-32-32-32L320 0zM80 32C35.8 32 0 67.8 0 112L0 432c0 44.2 35.8 80 80 80l320 0c44.2 0 80-35.8 80-80l0-112c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 112c0 8.8-7.2 16-16 16L80 448c-8.8 0-16-7.2-16-16l0-320c0-8.8 7.2-16 16-16l112 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L80 32z" fill="currentColor"></path>
+          </svg>
+        </a>
       </mvx-explorer-link>
     `);
   });
@@ -39,7 +47,11 @@ describe('ExplorerLink', () => {
 
     expect(page.root).toEqualHtml(`
       <mvx-explorer-link link="https://example.com" class="custom-class">
-        <a class="custom-class explorer-link mvx:decoration-0 mvx:flex" href="https://example.com" rel="noreferrer" target="_blank"></a>
+        <a class="custom-class explorer-link mvx:decoration-0 mvx:flex" href="https://example.com" rel="noreferrer" target="_blank">
+          <svg class="explorer-link-icon mvx:duration-200 mvx:ease-in-out mvx:flex mvx:hover:opacity-80 mvx:justify-center mvx:transition-opacity" height="16" viewBox="0 0 512 512" width="16" xmlns="http://www.w3.org/2000/svg">
+            <path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l82.7 0L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3l0 82.7c0 17.7 14.3 32 32 32s32-14.3 32-32l0-160c0-17.7-14.3-32-32-32L320 0zM80 32C35.8 32 0 67.8 0 112L0 432c0 44.2 35.8 80 80 80l320 0c44.2 0 80-35.8 80-80l0-112c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 112c0 8.8-7.2 16-16 16L80 448c-8.8 0-16-7.2-16-16l0-320c0-8.8 7.2-16 16-16l112 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L80 32z" fill="currentColor"></path>
+          </svg>
+        </a>
       </mvx-explorer-link>
     `);
   });
@@ -57,7 +69,11 @@ describe('ExplorerLink', () => {
             <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" fill="currentColor"></path>
           </svg>
         </mvx-arrow-right-icon>
-        <a class="explorer-link mvx:decoration-0 mvx:flex" href="https://example.com" rel="noreferrer" target="_blank"></a>
+        <a class="explorer-link mvx:decoration-0 mvx:flex" href="https://example.com" rel="noreferrer" target="_blank">
+          <svg class="explorer-link-icon mvx:duration-200 mvx:ease-in-out mvx:flex mvx:hover:opacity-80 mvx:justify-center mvx:transition-opacity" height="16" viewBox="0 0 512 512" width="16" xmlns="http://www.w3.org/2000/svg">
+            <path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l82.7 0L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3l0 82.7c0 17.7 14.3 32 32 32s32-14.3 32-32l0-160c0-17.7-14.3-32-32-32L320 0zM80 32C35.8 32 0 67.8 0 112L0 432c0 44.2 35.8 80 80 80l320 0c44.2 0 80-35.8 80-80l0-112c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 112c0 8.8-7.2 16-16 16L80 448c-8.8 0-16-7.2-16-16l0-320c0-8.8 7.2-16 16-16l112 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L80 32z" fill="currentColor"></path>
+          </svg>
+        </a>
       </mvx-explorer-link>
     `);
   });
